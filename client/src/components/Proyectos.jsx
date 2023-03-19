26abb8d0b40b825741776cfa0bcacd71727d0e0d
juampimas <juampim98@gmail.com>
2023-03-19 17:39:41 -0300

    Portfolio

--- client/src/components/Proyectos.jsx
+++ client/src/components/Proyectos.jsx
@@ -36,11 +36,11 @@ const [t] = useTranslation("global")
 
                     </div>
                     <div className="pro-desc">
-                        <h4>{t("proyectos.pro_item6_title")}</h4>
-                        <p>{t("proyectos.pro_item6_text")}</p>
+                        <h4>{t("proyectos.pro_item4_title")}</h4>
+                        <p>{t("proyectos.pro_item4_text")}</p>
                         <div className="pro-footer">
-                            <p><span>{t("proyectos.pro_item_year")}</span> {t("proyectos.pro_item6_year")}</p>
-                            <Link className='btn-site' to="https://www.animus.is/" target="_blank">
+                            <p><span>{t("proyectos.pro_item_year")}</span> {t("proyectos.pro_item4_year")}</p>
+                            <Link className='btn-site' to="https://juampimas.github.io/Lengua-madre/" target="_blank">
                                 <div className="pro-btn">
                                     <p>{t("proyectos.pro_item_btn")}</p>
                                 </div>
@@ -55,11 +55,11 @@ const [t] = useTranslation("global")
                         
                     </div>
                     <div className="pro-desc">
-                        <h4>{t("proyectos.pro_item5_title")}</h4>
-                        <p>{t("proyectos.pro_item5_text")}</p>
+                        <h4>{t("proyectos.pro_item6_title")}</h4>
+                        <p>{t("proyectos.pro_item6_text")}</p>
                         <div className="pro-footer">
-                            <p><span>{t("proyectos.pro_item_year")}</span> {t("proyectos.pro_item5_year")}</p>
-                            <Link to="https://juampimas.github.io/Animus/" target="_blank" className='btn-site'>
+                            <p><span>{t("proyectos.pro_item_year")}</span> {t("proyectos.pro_item6_year")}</p>
+                            <Link to="https://www.animus.is/" target="_blank" className='btn-site'>
                                 <div className="pro-btn">
                                     <p>{t("proyectos.pro_item_btn")}</p>
                                 </div>
@@ -70,6 +70,25 @@ const [t] = useTranslation("global")
                 <div className="pro-item">
                     <div className="pro-img">
 
+                    </div>
+                    <div className="pro-desc">
+                        <h4>{t("proyectos.pro_item5_title")}</h4>
+                        <p>{t("proyectos.pro_item5_text")}</p>
+                        <div className="pro-footer">
+                            <p><span>{t("proyectos.pro_item_year")}</span> {t("proyectos.pro_item5_year")}</p>
+                            <Link className='btn-site' to="https://juampimas.github.io/Animus/" target="_blank">
+                                <div className="pro-btn">
+                                    <p>{t("proyectos.pro_item_btn")}</p>
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="pro-sub-container">
+                <div className="pro-item">
+                    <div className="pro-img">
+                        
                     </div>
                     <div className="pro-desc">
                         <h4>{t("proyectos.pro_item2_title")}</h4>
@@ -84,35 +103,16 @@ const [t] = useTranslation("global")
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="pro-sub-container">
                 <div className="pro-item">
                     <div className="pro-img">
-                        
+
                     </div>
                     <div className="pro-desc">
                         <h4>{t("proyectos.pro_item3_title")}</h4>
                         <p>{t("proyectos.pro_item3_text")}</p>
                         <div className="pro-footer">
                             <p><span>{t("proyectos.pro_item_year")}</span> {t("proyectos.pro_item3_year")}</p>
-                            <Link className='btn-site' to="http://tmrepresentaciones.com/" target="_blank">
-                                <div className="pro-btn">
-                                    <p>{t("proyectos.pro_item_btn")}</p>
-                                </div>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-                <div className="pro-item">
-                    <div className="pro-img">
-
-                    </div>
-                    <div className="pro-desc">
-                        <h4>{t("proyectos.pro_item4_title")}</h4>
-                        <p>{t("proyectos.pro_item4_text")}</p>
-                        <div className="pro-footer">
-                            <p><span>{t("proyectos.pro_item_year")}</span> {t("proyectos.pro_item4_year")}</p>
-                            <Link to="https://github.com/Juampimas/Tienda-ecommerce" target="_blank" className='btn-site'>
+                            <Link to="http://tmrepresentaciones.com/" target="_blank" className='btn-site'>
                                 <div className="pro-btn">
                                     <p>{t("proyectos.pro_item_btn")}</p>
                                 </div>
